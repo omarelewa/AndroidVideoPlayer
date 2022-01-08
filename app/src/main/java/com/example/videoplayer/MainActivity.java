60e6109beeb5ab66bdd56d7da73e2cd81aecf3e6
Omar Elewa <elewa97@aucegypt.edu>
2022-01-08 17:35:43 +0200

    MainActivity implemented, only UI

--- app/src/main/java/com/example/videoplayer/MainActivity.java
+++ app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.videoplayer;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    ListView l;
+    String[] Genres = {"Adventure", "Drama", "Comedy", "Sci-Fi"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        l = findViewById(R.id.list);
+        ArrayAdapter<String> arr;
+        arr = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, Genres);
+        l.setAdapter(arr);
     }
 }
